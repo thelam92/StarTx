4171297cc4c98b82ec124911ccbdc16d5324a8c0
thelam92@gmail.com <Iloveyou1992>
2017-11-12 22:27:07 +0700

    initial commit

---
+++ StarTx_Shop/src/main/java/com/startx/StarTxShopApplication.java
@@ -0,0 +1,12 @@
+package com.startx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarTxShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarTxShopApplication.class, args);
+	}
+}
